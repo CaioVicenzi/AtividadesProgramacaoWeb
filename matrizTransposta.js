9ecfad4179f4bccee589ed4d371ea738c422f6d2
CAIO OLIVEIRA MARQUES <caio.marques@a.ucb.br>
2023-08-16 10:52:42 -0300

    colocando o inicio do que sera o algoritmo

---
+++ matrizTransposta.js
@@ -0,0 +1,16 @@
+var matrizA = [[1, 2], [3, 4], [5, 6]]
+
+var matrizB = []
+
+for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 2; j++) {
+        matrizB[i][j] = matrizA [i][j]
+
+    }
+}
+
+console.log("MATRIZ A: ")
+console.log(matrizA)
+
+console.log ("MATRIZ B: ")
+console.log (matrizB)
